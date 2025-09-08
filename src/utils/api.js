be695fee50d9f77e8e0f132fd08fc55072e51349
vladmelevich <vladmelevic@gmail.com>
2025-09-08 06:13:22 +0300

    Добавление IP адреса сервера в ALLOWED_HOSTS для доступа к админ панели

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import { getAuthToken, isTokenExpired, refreshToken } from './auth';
 
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = '/api';
 
 class ApiClient {
   constructor() {
